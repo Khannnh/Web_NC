634d549c632484a587e1fa69a1f740c2dad98dbf
Khannnh <hhonksnguyen2205@gmail.com>
2026-09-13 23:44:46 +0700

    khởi tạo dự án môn học

--- Buoi2/hello.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    return `Hello , ${name} !`;
-}
-const message = greet("Khánh");
-console.log(message);
-//# sourceMappingURL=hello.js.map
--- Buoi2/hello.ts
+++
@@ -1,5 +0,0 @@
-function greet(name:String): string {
-    return `Hello , ${name} !`;
-}
-const message: string = greet("Khánh");
-console.log(message)
